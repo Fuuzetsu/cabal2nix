ef09da559f49c38150118283f5665febf9f8f1d4
Peter Simons <simons@cryp.to>
2014-05-03 11:24:18 +0200

    release.nix: build the new version with 7.8.3 only for the time being

--- release.nix
+++ release.nix
@@ -3,7 +3,7 @@
 { cabal2nixSrc ? { outPath = ./.; revCount = 0; gitTag = "dirty"; }
 , releaseBuild ? false
 , supportedPlatforms ? if releaseBuild then ["i686-linux" "x86_64-linux" "x86_64-darwin"] else ["x86_64-linux"]
-, supportedCompilers ? ["ghc6104" "ghc6123" "ghc704" "ghc722" "ghc742" "ghc763" "ghc783" "ghcHEAD"]
+, supportedCompilers ? [/*"ghc6104" "ghc6123" "ghc704" "ghc722" "ghc742" "ghc763"*/ "ghc783" /*"ghcHEAD"*/]
 }:
 
 let
@@ -21,7 +21,7 @@ in
       version = cabal2nixSrc.gitTag;
       isLibrary = false;
       isExecutable = true;
-      buildDepends = with haskellPackages; [ Cabal filepath hackageDb HTTP mtl regexPosix ];
+      buildDepends = with haskellPackages; [ Cabal filepath hackageDb HTTP mtl regexPosix IfElse optparseApplicative ];
       testDepends = with haskellPackages; [ doctest ];
       doCheck = self.stdenv.lib.versionOlder "7.6" self.ghc.version;
       meta = {
